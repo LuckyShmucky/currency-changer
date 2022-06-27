47bd133c226a3b775340fc2ca0f74fe627203391
LuckyShmucky <christianluciano567@gmail.com>
2022-06-27 15:57:27 -0700

    meeting acceptance criteria

--- exchange.py
+++ exchange.py
@@ -28,41 +28,45 @@ class Currency:
     def __add__(self, other):
         # Defines the '+' operator. If other is a Currency object, the currency values are added and the result will be the unit of self. If other is an int or a float,
         # other will be treated as a USD value.
-        return Currency.__radd__(self, other)
-
-    def __iadd__(self, other):
-        return Currency.__add__(self, other)
-
-    def __radd__(self, other):
-        
         if isinstance(other, Currency) and other.unit == self.unit:
             return Currency(self.value + other.value, self.unit)
         elif isinstance(other, Currency) and other.unit != self.unit:
             convert_other = (
                 other.value / Currency.currencies[other.unit] * Currency.currencies[self.unit])
-            return Currency(convert_other + self.value, self.unit)
+            return Currency(round(convert_other + self.value, 2), self.unit)
         elif isinstance(other, int) or isinstance(other, float):
             other_intial = Currency(other, 'USD')
-            convert_initial = (other_intial.value / 
-                Currency.currencies[other_intial.unit] *
-                Currency.currencies[self.unit])
-            return Currency(self.value + convert_initial, self.unit), 'other is a num'
-        
+            convert_initial = (other_intial.value /
+                               Currency.currencies[other_intial.unit] *
+                               Currency.currencies[self.unit])
+            return Currency(round(self.value + convert_initial, 2), self.unit)
+
+    def __iadd__(self, other):
+        return Currency.__add__(self, other)
+
+    def __radd__(self, other):
+        if isinstance(other, Currency):
+            return Currency.__add__(self, other)
         else:
-            print('must be number or another currency to add')
-            return self.value, self.unit
+            other_currency = Currency(other, "USD")
+            new_self = Currency(self.value, self.unit)
+            new_self.changeTo("USD")
+            return Currency(round(new_self.value + other_currency.value, 2), other_currency.unit)
 
     def __sub__(self, other):
         if isinstance(other, Currency):
-            return Currency(self.value - other.value, self.unit)
+            return Currency(round(self.value - other.value, 2), self.unit)
         elif isinstance(other, int) or isinstance(other, float):
-            return Currency(self.value - other, self.unit)
+
+            other_currency = Currency(other, "USD")
+            other_currency.changeTo(self.unit)
+            return Currency(round(self.value - other_currency.value, 2), self.unit)
         else:
             print('must be number or another currency to add')
             return self.value, self.unit
 
     def __rsub__(self, other_num):
-        return Currency(other_num - self.value, self.unit)
+        return Currency(round(other_num - self.value, 2), self.unit)
 
 
 v1 = Currency(23.43, "EUR")
@@ -70,11 +74,11 @@ v2 = Currency(19.97, "USD")
 print('1', v1 + v2)
 print('2', v2 + v1)
 print('3', v1 + 3)
-# an int or a float is considered to be a USD value
+
 print('4', 3 + v1)
 print('5', v1 - 3)
-# print(v1 - v2)
-#  # an int or a float is considered to be a USD value
 print('6', 30 - v2)
+
+
 
 # print(type(1.1))
